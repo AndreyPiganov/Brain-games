d7d831915d5e874620bd27ce7cd319a7c06e44ab
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-17 17:08:56 +0300

    add src

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const UserHello = () =>{
+let name = readlineSync.question('May I have your name? ');
+let Hello= 'Hello, ' + name + '!';
+return Hello;
+}
+export {UserHello};
