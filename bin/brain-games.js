439d5e910bed67a453a836357a74477254789ca3
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-22 12:15:35 +0300

    fix files

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
 import { UserHello } from "../src/cli.js";
+
 console.log('Welcome to the Brain Games!');
 console.log(UserHello());
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const UserHello = () =>{
-let name = readlineSync.question('May I have your name? ');
-let Hello= 'Hello, ' + name + '!';
-return Hello;
+    let name = readlineSync.question('May I have your name? ');
+    let Hello= 'Hello, ' + name + '!';
+    return Hello;
 }
 export {UserHello};
