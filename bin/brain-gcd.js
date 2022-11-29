4fbbb3b27d5809f24f7854061807226b526eb0e9
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-29 15:03:58 +0300

    add new game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+import {
+  name2, min, max, NOD,
+} from '../src/index.js';
+
+let count = 0;
+console.log('Find the greatest common divisor of given numbers.');
+while ((count <= 2) && (count >= -5)) {
+  const random = Math.floor(Math.random() * (max - min + 1) + min);
+  const random2 = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(`Question: ${random} ${random2}`);
+  const answer = readlineSync.question('Your answer:');
+  const MaxDivisorRandom = NOD(random, random2);
+  if (MaxDivisorRandom == answer) {
+    console.log('Correct!');
+    count += 1;
+  } else {
+    console.log(`"${answer}" is wrong answer ;(. Correct answer was "${MaxDivisorRandom}".`);
+    count -= 10;
+    console.log(`Lets try again,${name2}!`);
+  }
+  if (count === 3) {
+    console.log(`Congratulations,${name2}`);
+  }
+}
