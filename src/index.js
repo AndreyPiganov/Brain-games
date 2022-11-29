1fb28083da14e9e821985d027e0a7644928b67f0
AndreyPiganov <andrey.piganov@mail.ru>
2022-11-29 15:05:13 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -12,6 +12,16 @@ const RandomElement = () => {
   const RandomOperation = (Bot, Top, arr) => arr[Math.floor(Math.random() * (Top - Bot + 1)) + Bot];
   return RandomOperation(0, answers.length - 1, answers);
 };
+const NOD = (a, b) => {
+  while (a !== b) {
+    if (a > b) {
+      a -= b;
+    } else {
+      b -= a;
+    }
+  }
+  return a;
+};
 export {
-  name2, min, max, RandomElement,
+  name2, min, max, RandomElement, NOD,
 };
