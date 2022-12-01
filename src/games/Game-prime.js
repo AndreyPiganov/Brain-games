1c382202f85b88c99d614b52952ca0af7e03b422
AndreyPiganov <andrey.piganov@mail.ru>
2022-12-01 14:10:23 +0300

    fix file

--- src/games/Game-prime.js
+++ src/games/Game-prime.js
@@ -4,7 +4,7 @@ import { name2, min, max } from '../index.js';
 const GamePrimeData = () => {
   let count = 0;
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  while ((count <= 2) && (count >= -5)) {
+  while ( count <= 2) {
     const random = Math.floor(Math.random() * (max - min + 1) + min);
     console.log(`Question: ${random}`);
     const answer = readlineSync.question('Your answer: ');
@@ -19,7 +19,7 @@ const GamePrimeData = () => {
     }
     if ((random === 1) && (answer === 'yes')) {
       console.log('"yes" is wrong answer ;(. Correct answer was "no".');
-      count -= 10;
+      count += 5;
       console.log(`Let's try again, ${name2}`);
     } else if ((random === 1) && (answer === 'no')) {
       console.log('Correct!');
@@ -29,17 +29,17 @@ const GamePrimeData = () => {
       count += 1;
     } else if ((counter2 === 0) && (answer === 'no')) {
       console.log('"no" is wrong answer ;(. Correct answer was "yes".');
-      count -= 10;
+      count += 5;
       console.log(`Let's try again,${name2}`);
     } else if ((counter2 !== 0) && (answer === 'yes')) {
       console.log('"yes" is wrong answer ;(. Correct answer was "no".');
-      count -= 10;
+      count += 5;
       console.log(`Let's try again,${name2}`);
     } else if ((counter2 !== 0) && (answer === 'no')) {
       console.log('Correct!');
       count += 1;
     } else {
-      count -= 10;
+      count += 5;
       console.log('Something went wrong :)');
     }
     if (count === 3) {
