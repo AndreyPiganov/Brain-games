2de15d6d7a083f8b57d30185c314033eb683fbbf
AndreyPiganov <andrey.piganov@mail.ru>
2022-12-02 15:35:35 +0300

    fix file

--- src/games/Game-prime.js
+++ src/games/Game-prime.js
@@ -3,19 +3,13 @@ import { getRandomInt, getRandomPrime } from '../helpers.js';
 
 const descriptionGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const GamePrimeData = () => {
-  const answer = ['yes', 'no'];
   const question = getRandomInt();
   const indexPrime = getRandomPrime(question);
-  if (indexPrime === 1) {
-    return [question, answer[1]];
-  }
-  if (question === 1) {
-    return [question, answer[1]];
-  }
   if (indexPrime === 0) {
-    return [question, answer[0]];
+    return [question, 'yes'];
   }
-  return false;
+
+  return [question, 'no'];
 };
 export default function primeGame() {
   startGame(descriptionGame, GamePrimeData);
