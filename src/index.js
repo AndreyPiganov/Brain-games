d6759db348f4eb40467d07f3925184e04e8802a4
AndreyPiganov <andrey.piganov@mail.ru>
2022-12-02 15:11:58 +0300

    fix file

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ export default function startGame(descriptionGame, getGame) {
     const userAnswer = readlineSync.question('Your answer:');
     if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again,${name}!`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
     console.log('Correct!');
