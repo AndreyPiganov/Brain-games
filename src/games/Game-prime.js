0b2680b0195c760fd569e6cffbeb4068213839ff
AndreyPiganov <andrey.piganov@mail.ru>
2023-03-16 15:28:48 +0300

    fix game-prime

--- src/games/Game-prime.js
+++ src/games/Game-prime.js
@@ -3,7 +3,7 @@ import { getRandom, getRandomPrime } from '../helpers.js';
 
 const descriptionGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const primeData = () => {
-  const question = getRandom();
+  const question = getRandom(2);
   const indexPrime = getRandomPrime(question);
   if (indexPrime === 0) {
     return [question, 'yes'];
